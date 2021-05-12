7ab0c563504490d10887cd10ddd10c4bc6da9637
Ali Hasan <alihasan.iubat@gmail.com>
2021-05-12 23:34:42 +0600

    Added prismjs example

--- kb-articles/1120/VPN.ps1
+++ kb-articles/1120/VPN.ps1
@@ -3,10 +3,10 @@ Start-Sleep -s 10
 
 # Map G Drive
 if (-not(get-psdrive -name "G" -ErrorAction SilentlyContinue)) {
-New-PSDrive -name "G" -PSProvider FileSystem -Root \\Server\share1 -Persist
+	New-PSDrive -name "G" -PSProvider FileSystem -Root \\Server\share1 -Persist
 }
 
 # Map H Drive
 if (-not(get-psdrive -name "H" -ErrorAction SilentlyContinue)) {
-New-PSDrive -name "H" -PSProvider FileSystem -Root \\Server\Share2 -Persist
+	New-PSDrive -name "H" -PSProvider FileSystem -Root \\Server\Share2 -Persist
 } 
